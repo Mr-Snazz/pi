610ae8b0f4a58d5fcde89bf7e48c1fcc4ef2d519
Johnny Bliss <mr_snazz@outlook.com>
2026-06-07 16:28:20 -0700

    Latest Changes

--- src/core/main-loop.cpp
+++ src/core/main-loop.cpp
@@ -1,6 +1,14 @@
+#include <wiringPi.h>
+
 #include "core/core.hpp"
 
 void SPI::MainLoop()
 {
-    
+    while (true)
+    {
+        digitalWrite(17, HIGH);
+        delay(500);
+        digitalWrite(17, LOW);
+        delay(500);
+    }
 }
