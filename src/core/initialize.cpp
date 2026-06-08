e281b0ba81a26e8d6eb8099c3d55417ff7da588f
Johnny Bliss <mr_snazz@outlook.com>
2026-06-07 17:08:22 -0700

    Latest Changes

--- src/core/initialize.cpp
+++ src/core/initialize.cpp
@@ -3,10 +3,14 @@
 #include <wiringPi.h>
 
 #include "core/core.hpp"
+#include "pins.hpp"
 
 void SPI::Initialize()
 {
     if (wiringPiSetupGpio() == -1) throw std::runtime_error("Failed to initialize wiringPi");
 
-   
+    for (uint8_t I = 0u; I < SPI::TotalPins; ++I) {
+        pinMode(SPI::InputPins[I], INPUT);
+        pullUpDnControl(SPI::InputPins[I], PUD_UP);
+    }
 }
