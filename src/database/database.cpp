d5e30db501766d115a94267bb3a281c75e3d5c8b
Johnny Bliss <mr_snazz@outlook.com>
2026-06-12 20:35:29 -0700

    Latest Changes

--- src/database/database.cpp
+++ src/database/database.cpp
@@ -4,10 +4,11 @@
 
 #include "database/database.hpp"
 
-constexpr const char* CreateTableQuery = "CREATE TABLE IF NOT EXISTS USERS("
-                                         "ID INTEGER PRIMARY KEY AUTOINCREMENT, "
-                                         "NAME TEXT NOT NULL, "
-                                         "AGE INT NOT NULL);";
+constexpr const char* CreateEventTableQuery =
+    "CREATE TABLE IF NOT EXISTS EVENTS ("
+    "ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+    "NAME TEXT NOT NULL, "
+    "PINS_TO_TRIGGER TEXT NOT NULL);";
 
 constexpr const char* FailedToOpenDatabaseError = "Failed to open or create database file\n";
 
@@ -17,16 +18,18 @@ sqlite3* DB;
 
 void SPI::Database::Initialize()
 {
-    if (sqlite3_open("test.db", &DB) != SQLITE_OK) throw std::runtime_error(FailedToOpenDatabaseError);
+    if (sqlite3_open("databases/test.db", &DB) != SQLITE_OK) throw std::runtime_error(FailedToOpenDatabaseError);
 
-    char* ErrorMessage = nullptr;
-    if (sqlite3_exec(DB, CreateTableQuery, nullptr, 0, &ErrorMessage) != SQLITE_OK) {
-        throw std::runtime_error(FailedToExecuteCreationQueryError);
+    char* ErrorMessage{nullptr};
+    if (sqlite3_exec(DB, CreateEventTableQuery, nullptr, 0, &ErrorMessage) != SQLITE_OK) {
         sqlite3_free(ErrorMessage);
+        throw std::runtime_error(FailedToExecuteCreationQueryError);
     }
 
-    const char* Query = "INSERT INTO USERS (NAME, AGE) VALUES('Alice', 25);";
-    sqlite3_exec(DB, Query, [](void* NotUsed, int argc, char** argv, char** azColName) -> int { return 0; }, 0, &ErrorMessage);
+    const char* Query =
+    "INSERT INTO EVENTS (NAME, PINS_TO_TRIGGER) "
+    "VALUES ('Test', '[1,2,3]');";
+    sqlite3_exec(DB, Query, [](void* NotUsed, int argc, char** argv, char** azColName){ return 0; }, 0, &ErrorMessage);
 }
 
 void SPI::Database::FreeResources()
